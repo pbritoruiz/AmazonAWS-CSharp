44202fbc32f66225cb5061c33e3bd8989b2a591b
Paulo Henrique de Brito Ruiz <pauloruiz.sp@gmail.com>
2017-10-04 15:27:51 -0300

    First Version

---
+++ S3Upload/S3Upload/AmazonS3Uploader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace S3Upload
+{
+    public class AmazonS3Uploader
+    {
+        public void UploadFile(String path)
+        {
+            using (IAmazonS3 s3Client = Amazon.AWSClientFactory.CreateAmazonS3Client(
+                    "MY ACCESS KEY", "MY SECRET ACCESS KEY", Amazon.RegionEndpoint.USEast1))
+            {
+                PutObjectRequest request = new PutObjectRequest();
+
+                request.BucketName = "s3uploadcsharp";
+                request.CannedACL = S3CannedACL.PublicRead;
+                request.Key = Path.GetFileName(path);
+                request.FilePath = path;
+
+                s3Client.PutObject(request);
+            }
+        }
+    }
+}
---
+++ S3Upload/S3Upload/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace S3Upload
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            String path = @"C:\Upload\CSharp";
+
+            Console.WriteLine(String.Concat("Monitoring directory: ", path));
+
+            String[] files = Directory.GetFiles(path);
+
+            if (files != null)
+            {
+                AmazonS3Uploader amazonS3Uploader = new AmazonS3Uploader();
+
+                foreach (String file in files)
+                {
+                    Console.WriteLine(String.Concat("Uploading files: " + Path.GetFileName(file)));
+
+                    amazonS3Uploader.UploadFile(file);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
